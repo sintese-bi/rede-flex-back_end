60deb9c2a21673c92f6cd076278ce305044ff59b
Eloy <eloymjunior00@gmail.com>
2024-05-20 17:21:34 -0300

    General Configs

---
+++ src/Controllers/UserController.ts
@@ -0,0 +1,32 @@
+
+import nodemailer from 'nodemailer';
+import { Prisma } from "@prisma/client";
+import { Request, Response, application } from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+// import moment from "moment"
+// import cron from "node-cron"
+import jwt from 'jsonwebtoken';
+const prisma = new PrismaClient();
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+
+    secure: true,//Usar "false" para ambiente de desenvolvimento
+    auth: {
+        user: "",
+        pass: process.env.PASSGMAIL,
+    },
+    tls: {
+        rejectUnauthorized: true, //Usar "false" para ambiente de desenvolvimento
+    },
+});
+
+
+
+class UserController { }
+
+
+
+export default new UserController()
