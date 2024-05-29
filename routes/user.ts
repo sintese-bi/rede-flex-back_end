88a5591c1947fdbd181f40eb7616e76428eba145
Eloy <eloymjunior00@gmail.com>
2024-05-29 16:00:23 -0300

    Use Flow Step 7

--- routes/user.ts
+++ routes/user.ts
@@ -6,4 +6,5 @@ const userRouter = Router()
 userRouter.post("/register", UserController.registerUsers)
 userRouter.post("/login", UserController.login)
 userRouter.post(`/sendemail`, UserController.sendEmail)
+userRouter.post(`/password_recovery`, UserController.passwordRecover)
 export default userRouter
