ecabf7c28c6be103cea024a62b12ce98bd666907
Eloy <eloymjunior00@gmail.com>
2024-06-17 12:00:44 -0300

    general

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import variableRouter from "../routes/variable";
 import userRouter from "../routes/user";
 // import wppRouter from "../routes/wpp";
 const app = express();
-const PORT = 8080;
+const PORT = 3051;
 const apiVersion = "v1";
 
 const corsOptions = {
