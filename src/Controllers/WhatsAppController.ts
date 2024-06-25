64e9153f822d2d349dc26365e874983452c22a7c
Eloy <eloymjunior00@gmail.com>
2024-06-25 13:43:05 -0300

    Alert Flow

--- src/Controllers/WhatsAppController.ts
+++ src/Controllers/WhatsAppController.ts
@@ -1,23 +1,23 @@
-// import { Prisma } from "@prisma/client";
-// import { Request, Response, application } from "express";
-// import { PrismaClient } from "@prisma/client";
-// import { MongoClient, ServerApiVersion } from 'mongodb';
-// import PDFDocument from "pdfkit";
-// import fs from "fs";
+import { Prisma } from "@prisma/client";
+import { Request, Response, application } from "express";
+import { PrismaClient } from "@prisma/client";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import PDFDocument from "pdfkit";
+import fs from "fs";
 // import { createCanvas } from "canvas";
-// import { Chart, registerables } from "chart.js";
-// Chart.register(...registerables);
-// const uri: string | undefined = process.env.MONGODBCREDENCIALS
-// let client: any
-// if (uri) {
-//     client = new MongoClient(uri, {
-//         serverApi: {
-//             version: ServerApiVersion.v1,
-//             strict: true,
-//             deprecationErrors: true,
-//         }
-//     });
-// }
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+const uri: string | undefined = process.env.MONGODBCREDENCIALS
+let client: any
+if (uri) {
+    client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+}
 // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 // let data: any = {
 //     unidade: "Felipe VGP - 4 kWp",
@@ -50,20 +50,22 @@
 // };
 // const prisma = new PrismaClient();
 
-// class WhatsAppController {
-//     public async wppInfo() {
-//         try {
-//             // Connect the client to the server	(optional starting in v4.7)
-//             await client.connect();
-//             // Send a ping to confirm a successful connection
-//             await client.db("admin").command({ ping: 1 });
-//             console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//         } finally {
-//             // Ensures that the client will close when you finish/error
-//             await client.close();
-//         }
-
-//     }
+class WhatsAppController {
+    public async wppInfo() {
+        try {
+            // Connect the client to the server (optional starting in v4.7)
+            await client.connect();
+            // Send a ping to confirm a successful connection
+            await client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        } catch (error) {
+            // Handle the error here
+            console.error("An error occurred while connecting to MongoDB:", error);
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
+    }
 //     public async createPDF(): Promise<void> {
 //         function createChart(
 //             data: number[],
@@ -198,11 +200,11 @@
 //         doc.end();
 
 
-//     }
+    }
 
 
 // }
-// const teste = new WhatsAppController()
+const teste = new WhatsAppController()
 
-// // teste.wppInfo().catch(console.dir);
-// export default new WhatsAppController()
+// teste.wppInfo().catch(console.dir);
+export default new WhatsAppController()
